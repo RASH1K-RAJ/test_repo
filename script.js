db347a92cb4118fb75be1cd82eb6afe83b066247
RASH1k-RAJ <pommyraj@gmail.com>
2023-07-04 11:13:02 +1000

    revision_edit

---
+++ script.js
@@ -0,0 +1,4 @@
+let main_body = document.querySelector(".container");
+console.log(main_body.style.cssText);
+main_body.style.cssText = 'border: 5px solid green;';
+console.log(main_body.style.cssText);
